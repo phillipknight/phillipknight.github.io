7edc85b6dcef8e997afd0d0c5abab834ebfc92cb
phillipknight <phillipkknight@gmail.com>
2020-02-09 20:40:31 -0500

    update site name, favicon, begin index layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,24 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
+    <h1>Phillip Kent Knight</h1>
+    <ul>
+      <li>Campaigner</li>
+      <li>Developer</li>
+      <li>Product Manager</li>
+    </ul>
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
-
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   }
-}
+};
 </script>
 
 <style>
