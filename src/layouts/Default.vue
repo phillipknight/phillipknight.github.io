4298413099d6d8be9ad27a38f657f212493e0644
phillipknight <phillipkknight@gmail.com>
2020-02-09 22:09:39 -0500

    Revert "remove starter styles"
    This reverts commit 437980395181c8dfbb54814e1138ee42c79471e9.
    
    fix fuckup

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -22,9 +22,19 @@ query {
 </static-query>
 
 <style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
 
-
-
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
 
 .header {
   display: flex;
