924f3e21094d7905679793ba7406f987c3ac4296
phillipknight <phillipkknight@gmail.com>
2020-02-15 20:07:29 -0500

    try to work back

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Add static files here. Files in this directory will be copied directly to `dist` folder during build. For example, /static/robots.txt will be located at https://yoursite.com/robots.txt.
+# _Starting from_ Default starter for Gridsome
 
-This file should be deleted.
+This is the project you get when you run `gridsome create new-project` and then begin to reimagine phillipkentknight.com in small bursts of free time.
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <h1>Phillip Kent Knight</h1>
+    <ul>
+      <li>Advocacy Technologist</li>
+      <li>Campaigner</li>
+      <li>Developer</li>
+      <li>Product Manager</li>
+    </ul>
+    <p class="home-links">
+      <a href="https://twitter.com/phillipkent" target="_blank" rel="noopener">Twitter</a>
+      <a href="https://linkedin.com/in/phillipkentknight" target="_blank" rel="noopener">Linkedin</a>
+      <a href="https://instagram.com/phillipkent" target="_blank" rel="noopener">Instagram</a>
+      <a href="https://dev.to/phillipkent" target="_blank" rel="noopener">Dev Community</a>
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
